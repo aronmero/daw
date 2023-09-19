ff0bbab4edd311212f95ddb0015a9e8fb1e062c9
aaron <correo@correo.com>
2023-09-19 16:47:25 +0100

    UT2 Ejercicios 1 Completos

---
+++ UT2/E1/11.js
@@ -0,0 +1,6 @@
+for (j = 1; j < 50; j++) {
+  for (i = 1; i < j; i++) {
+    document.write(i);
+  }
+  document.write('<br>');
+}
---
+++ UT2/E1/2.js
@@ -0,0 +1,14 @@
+let variable1;
+let variable2;
+let variable3;
+
+let variable4="Aaron";
+let variable5= 24;
+let variable6=true;
+
+document.write(typeof(variable1)+" ");
+document.write(typeof(variable2)+" ");
+document.write(typeof(variable3)+" ");
+document.write(typeof(variable4)+" ");
+document.write(typeof(variable5)+" ");
+document.write(typeof(variable6));
