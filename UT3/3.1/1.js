039b4182604dfcccbf619740ea3696f944a6f7e0
aaron <correo@correo.com>
2023-09-28 17:55:06 +0100

    UT3 E3

--- UT3/3.1/1.js
+++ UT3/3.1/1.js
@@ -1,12 +1,14 @@
-//let ventana=open("https://perezpulido.org/","","width=1000,height=1000");
+let ventana =open("", "", "width=1000,height=1000");
 let porcentaje = parseInt(
-  prompt("Que porcentaje quiere aplicar para redimensionar la ventana", "50")
+  ventana.prompt("Que porcentaje quiere aplicar para redimensionar la ventana", "50")
 );
-
 if (!isNaN(porcentaje)) {
-  if (confirm("¿Redimensionar al " + porcentaje + "%?")) {
+  if (ventana.confirm("¿Redimensionar al " + porcentaje + "%?")) {
     porcentaje = porcentaje / 100;
-    document.write(porcentaje);
 
+    let widthVentana =ventana.outerWidth*porcentaje;
+    let heightVentana =ventana.outerHeight*porcentaje;
+
+    ventana.resizeTo(widthVentana,heightVentana);
   }
 }
--- UT3/3.1/3.js
+++ UT3/3.1/3.js
@@ -1,15 +1,14 @@
+let tTrabajo;
+let tDescanso;
 let tRestanteMinuto;
 let tRestanteSegundo;
 let textoImprimir;
 let tipoTiempo = document.getElementById("tipoTiempo");
-let tTrabajo;
 let intervalo;
 let isIniciado;
 let isTrabajo;
 
-intervalo = setInterval(function () {
-  self.actualizarTRestante.apply(self);
-}, 1000);
+intervalo = setInterval(function () {actualizarTRestante();}, 1000);
 
 function iniciar() {
   isIniciado = true;
@@ -18,25 +17,21 @@ function iniciar() {
   tipoTiempo.innerHTML = "Tiempo de concentracion";
   tRestanteMinuto = tTrabajo;
   tRestanteSegundo = 0;
-  
 }
 
+//Obtener valores del html
 function obtenerValores() {
-  //Obtiene el valor del input del html
   tTrabajo = document.getElementById("tTrabajo").value;
-  //Asigna el valor default si es necesario
   if (tTrabajo == "") {
     tTrabajo = 0;
   }
-  //Obtiene el valor del input del html
   tDescanso = document.getElementById("tDescanso").value;
-  //Asigna el valor default si es necesario
   if (tDescanso == "") {
     tDescanso = 0;
   }
 }
 
-
+//Imprime los valores en el html
 function actualizarTRestante() {
   if (isIniciado) {
     if (isTrabajo) {
@@ -44,13 +39,13 @@ function actualizarTRestante() {
     } else {
       tipoTiempo.innerHTML = "Tiempo de descanso";
     }
-    //Anadir cer de relleno a la izquierda
+    //Anadir cero de relleno a la izquierda
     if (tRestanteMinuto > 10) {
       textoImprimir = tRestanteMinuto;
     } else {
       textoImprimir = "0" + tRestanteMinuto;
     }
-    //Anadir cer de relleno a la izquierda
+    //Anadir cero de relleno a la izquierda
     if (tRestanteSegundo > 10) {
       textoImprimir = textoImprimir + ":" + tRestanteSegundo;
     } else {
@@ -61,12 +56,14 @@ function actualizarTRestante() {
   }
 }
 
+//Reduce los contadores de tiempo
 function reducirTRestante() {
   let reduccion = 1; //Controlar la velocidad a la que disminuye el tiempo
   if (tRestanteSegundo - reduccion <= 0 && tRestanteMinuto > 0) {
     tRestanteMinuto = tRestanteMinuto - 1;
     tRestanteSegundo = 60 - reduccion;
   } else if (tRestanteSegundo - reduccion <= 0 && tRestanteMinuto == 0) {
+    //Cambio de un tiempo al otro
     if (isTrabajo) {
       isTrabajo = false;
       tRestanteMinuto = tDescanso;
