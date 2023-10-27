d32d763d57bd042292d61e7bffb441fee90ea389
aaron <aaron@daw.com>
2023-10-27 19:51:51 +0100

    ProyectosFormularios Inicio

---
+++ UT5/ProyectoFormularios/TestTelevision/1.js
@@ -0,0 +1,30 @@
+let puntuacion;
+let pregunta;
+let propuesta1;
+let propuesta2;
+let propuesta3;
+let propuesta4;
+function crearPregunta(params) {
+    document.createElement("div");
+
+ 
+}
+crearPregunta();
+
+/*
+<div class="pregunta" id="pregunta1">
+  <div>
+    <p>Puntuacion</p>
+    <p>1.00</p>
+  </div>
+  <div>
+    <form>
+      <label>${pregunta}</label>
+      <input type="radio" />
+      <input type="radio" />
+      <input type="radio" />
+      <input type="radio" />
+    </form>
+  </div>
+</div>;
+*/
