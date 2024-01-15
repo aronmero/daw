d7cec474169b5b017f36e7a624abc60bbb95d358
aaron <aaron@daw.com>
2024-01-15 14:29:34 +0000

    Organizar componentes

--- UT8/Curriculum/src/App.vue
+++ UT8/Curriculum/src/App.vue
@@ -1,21 +1,9 @@
 <script setup>
-import Formacion from "./components/Formacion.vue";
-import Titulo from "./components/Titulo.vue";
-import Contacto from "./components/Contacto.vue";
-import Idiomas from "./components/Idiomas.vue";
+import Persona from "./components/Persona.vue";
 </script>
 
 <template>
-  <h1>Aarón Medina Rodríguez</h1>
-  <Titulo titulo="Desarrollador Web"/>
-  <Contacto email="aronmedinasp@gmail.com" github="aronmero"/>  <br>
-  <h3>Formación</h3>
-  <Formacion />  <br>
-  <Idiomas/>  <br>
-  <Contacto email="aronmedinasp@gmail.com" github="aronmero"/>
- 
+  <Persona nombre="Aarón Medina Rodríguez"/>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
