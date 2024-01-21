908745f91a9da2eedc1f3d7959de9f7c1b9d6a62
aronmero <aronmedinasp@gmail.com>
2024-01-21 18:25:19 +0000

    Api nueva impresion ordenada

--- UT7/Api/main.js
+++ UT7/Api/main.js
@@ -1,53 +1,76 @@
 let pokedex = new Array();
 let pokedexFiltrada = [];
 let numPokemon = 0;
-
+let numPokemonTotal = 1302;
 if (sessionStorage.getItem("pokedex") != undefined) {
-  const json_str = sessionStorage.getItem("pokedex");
-  pokedex = JSON.parse(json_str);
+  reasignarPokedex();
+
   pokedex.forEach((pokemon) => {
     imprimirDatos(pokemon);
   });
+  if (numPokemon < numPokemonTotal) {
+    const error = document.createElement("span");
+    error.append(document.createTextNode(" *Se recomienda refrescar la api"));
+    error.classList.add("error");
+    document.getElementById("numPokedex").appendChild(error);
+  }
 } else {
-  pokedex = pedirDatos();
+  pokedex = inicio();
 }
 
-/**
- * Solicitar datos a la API y devolverlos
- * @date 12/4/2023 - 3:57:33 PM
- * @author Aaron Medina Rodriguez
- *
- */
 function pedirDatos() {
-  const pokedex = new Array();
-
-  fetch("https://pokeapi.co/api/v2/pokedex/1")
+  const pokedex = JSON.parse(sessionStorage.getItem("pokedex")) || new Array();
+  fetch("https://pokeapi.co/api/v2/pokemon/?offset=00&limit=15000")
     .then((response) => response.json())
-    .then((data) =>
-      data.pokemon_entries.forEach((element) => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${element.pokemon_species.name}`)
+    .then((pokemonList) =>
+      pokemonList.results.forEach((element) => {
+        fetch(`${element.url}`)
           .then((response) => response.json())
           .then((data) => {
             const pokemon = extraerDatos(data);
-            pokedex.push(pokemon);
-            imprimirDatos(pokemon);
-            sessionStorage.setItem("pokedex", JSON.stringify(pokedex));
+            const pokemonRepetido = pokedex.some((p) => p.id === pokemon.id);
+
+            if (!pokemonRepetido) {
+              pokedex.push(pokemon);
+              sessionStorage.setItem("pokedex", JSON.stringify(pokedex));
+            }
           })
-          .catch(() => console.warn("Error"));
+          .catch(() => console.warn("Error al obtener detalles del pokemon"));
       })
     )
-    .catch(() => console.warn("Error"));
+    .catch(() => console.warn("Error al obtener la lista de pokemon"));
+}
 
+function extraerDatos(pokemon) {
+  const pokemonTemp = {
+    id: pokemon.id,
+    sprite: pokemon.sprites.front_default,
+    nombre: pokemon.name,
+    tipos: pokemon.types,
+  };
+  return pokemonTemp;
+}
+
+function inicio() {
+  pedirDatos();
+  reasignarPokedex();
+  pokedex.forEach((pokemon) => {
+    imprimirDatos(pokemon);
+  });
+  if (numPokemon < numPokemonTotal) {
+    const error = document.createElement("span");
+    error.append(document.createTextNode(" *Se recomienda refrescar la api"));
+    error.classList.add("error");
+    document.getElementById("numPokedex").appendChild(error);
+  }
   return pokedex;
 }
 
-function extraerDatos(data) {
-  const pokemon = {
-    sprite: data.sprites.front_default,
-    nombre: data.name,
-    tipos: data.types,
-  };
-  return pokemon;
+function reasignarPokedex() {
+  const json_str = sessionStorage.getItem("pokedex");
+  pokedex = JSON.parse(json_str);
+  pokedex.sort((a, b) => a.id - b.id);
+  sessionStorage.setItem("pokedex", JSON.stringify(pokedex));
 }
 
 function imprimirDatos(data) {
@@ -58,6 +81,9 @@ function imprimirDatos(data) {
 
   if (data.sprite != null) {
     sprite.setAttribute("src", data.sprite);
+    sprite.onerror = function () {
+      sprite.setAttribute("src", "./media/default.webp");
+    };
   } else {
     sprite.setAttribute("src", "./media/default.webp");
   }
@@ -71,7 +97,8 @@ function imprimirDatos(data) {
   pokemon.addEventListener("click", mostrarInfo);
   container.append(pokemon);
   numPokemon++;
-  document.getElementById("numPokedex").innerHTML = "Numero de pokemon: " + numPokemon;
+  document.getElementById("numPokedex").innerHTML =
+    "Numero de pokemon cargdos " + numPokemon + " de " + numPokemonTotal;
 }
 
 /**
@@ -93,11 +120,15 @@ function filtrarPokemon() {
     //Comprueba el otro filtro y utiliza sus datos si el filtro tiene alguno
     if (pokedexFiltrada.length > 0) {
       filtrado = pokedexFiltrada.filter((rows) =>
-        rows.childNodes[1].textContent.toLowerCase().includes(this.value.toLowerCase())
+        rows.childNodes[1].textContent
+          .toLowerCase()
+          .includes(this.value.toLowerCase())
       );
     } else {
       filtrado = Array.from(rows).filter((rows) =>
-        rows.childNodes[1].textContent.toLowerCase().includes(this.value.toLowerCase())
+        rows.childNodes[1].textContent
+          .toLowerCase()
+          .includes(this.value.toLowerCase())
       );
     }
 
@@ -134,14 +165,27 @@ function mostrarInfo() {
   info[0].tipos.forEach((element) => {
     tipos = tipos + element.type.name + " ";
   });
-  const mensaje = "El nombre es " + nombre + "\nLos tipos son " + tipos;
+  const sprite = info[0].sprite;
+  const mensaje =
+    "El nombre es " +
+    nombre +
+    "\nLos tipos son " +
+    tipos +
+    "\n sprite:" +
+    sprite;
   alert(mensaje);
 }
 
-document.getElementById("busquedaPokemon").addEventListener("input", filtrarPokemon);
+document
+  .getElementById("busquedaPokemon")
+  .addEventListener("input", filtrarPokemon);
 document
   .getElementById("refrescarApi")
   .addEventListener(
     "click",
-    () => (((numPokemon = 0), (document.getElementById("containerDisplay").innerHTML = "")), pedirDatos())
+    () => (
+      ((numPokemon = 0),
+      (document.getElementById("containerDisplay").innerHTML = "")),
+      inicio()
+    )
   );
--- UT7/Api/readme.md
+++ UT7/Api/readme.md
@@ -1,16 +1,37 @@
 # Enunciado
+
 Selecciona una API publica de tu elección (puedes escoger una del repositorio:
 https://github.com/public-apis/public-apis ) y genera una interfaz con los siguientes requisitos:
 
 Usa fecth para obtener los datos desde la API.
 Utilizar .map() para representar cada uno de los elementos obtenidos de la API. (Modificando el DOM, creando nuevos nodos, intentar evitar usar el innerHTML...)
 Utilizar .filter() para realizar busquedas de items.
-Utilizar Cookies, LocalStorage, sessionStorage para implementar una funcionalidad de tu interfaz. 
-Añade 4 funcionalidades a tu aplicación web (a tu libre elección) usando javascript. 
-# Resolucion
-Se eligio la api de pokemon centrada en la pokedex *https://pokeapi.co/api/v2/pokedex/*.
+Utilizar Cookies, LocalStorage, sessionStorage para implementar una funcionalidad de tu interfaz.
+Añade 4 funcionalidades a tu aplicación web (a tu libre elección) usando javascript.
 
-Utilizamos un doble fecth para obtener los datos que requerimos de la api.
+# Resolucion
+
+Se eligio la api de pokemon que contiene todos los pokemon *https://pokeapi.co/api/v2/pokemon/?offset=00&limit=15000*.
+
+Utilizamos un doble fecth para obtener los datos que requerimos de la api. El primero obtiene de un objeto con el siguiente formato la url del pokemon concreto.
+
+```json
+{
+  "name": "bulbasaur",
+  "url": "https://pokeapi.co/api/v2/pokemon/1/"
+}
+```
+
+El segundo fetch obtiene el resto de datos del pokemon principalmente extraemos los siguientes datos para trabajar en nuestra app.
+
+```json
+{
+    "id": "pokemon.id",
+    "sprite": "pokemon.sprites.front_default",
+    "nombre": "pokemon.name",
+    "tipos": "pokemon.types",
+  }
+```
 
 Imprimimos cada elemento de la api individualmente en el HTML.
 
