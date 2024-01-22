265bb8c03654080b2e5d903eef8d19fc9cfab31b
aaron <aaron@daw.com>
2024-01-22 15:37:18 +0000

    Api conf basica

--- UT8/API/src/main.js
+++ UT8/API/src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
+
+  const { worker } = await import("./mocks/browser");
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  createApp(App).mount("#app");
+});
