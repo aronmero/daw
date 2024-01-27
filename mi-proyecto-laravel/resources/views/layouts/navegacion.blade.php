0b74c175088ffe2200b651c9cec3eaa4bf8ea857
aronmero <aronmedinasp@gmail.com>
2024-01-27 19:44:04 +0000

    nav seleccion actual fix

--- mi-proyecto-laravel/resources/views/layouts/navegacion.blade.php
+++ mi-proyecto-laravel/resources/views/layouts/navegacion.blade.php
@@ -1,8 +1,8 @@
 <div class="navegacion"> 
     @can('admin.usuario.index')
-        <a href="{{ route('profesores.index') }}" class="{{ request()->routeIs('profesores.index') ? 'activo':'' }} ">Profesores</a>
+        <a href="{{ route('profesores.index') }}" class="{{ request()->routeIs('profesores.*') ? 'activo':'' }} ">Profesores</a>
     @endcan
 
-    <a href="{{ route('actividades.index') }}" class="{{ request()->routeIs('actividades.index') ? 'activo':'' }} ">Actividades</a>
-    <a href="{{ route('grupos.index') }}" class="{{ request()->routeIs('grupos.index') ? 'activo':'' }} ">Grupos</a>
+    <a href="{{ route('actividades.index') }}" class="{{ request()->routeIs('actividades.*') ? 'activo':'' }} ">Actividades</a>
+    <a href="{{ route('grupos.index') }}" class="{{ request()->routeIs('grupos.*') ? 'activo':'' }} ">Grupos</a>
 </div> 
