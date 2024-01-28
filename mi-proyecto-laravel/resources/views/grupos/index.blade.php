e078727d92153d1fd34112d4df07388c9067de62
aronmero <aronmedinasp@gmail.com>
2024-01-28 10:19:05 +0000

    estilo index grupos y profesores

--- mi-proyecto-laravel/resources/views/grupos/index.blade.php
+++ mi-proyecto-laravel/resources/views/grupos/index.blade.php
@@ -7,40 +7,41 @@
     @can('admin.grupo.create')
         <a class="accion" href="{{ route('grupos.create') }}">Crear Grupo</a>
     @endcan
-    <table>
-        <thead>
-            <td>Nombre</td>
-            @canany(['admin.grupo.edit', 'admin.grupo.destroy', 'admin.grupo.show'])
-                <td colspan="3">Acciones</td>
-            @endcanany
-        </thead>
+    <div>
+        <table>
+            <thead>
+                <td>Nombre</td>
+                @canany(['admin.grupo.edit', 'admin.grupo.destroy', 'admin.grupo.show'])
+                    <td colspan="3">Acciones</td>
+                @endcanany
+            </thead>
 
-        @forelse ($grupos as $grupo)
-            <tr>
-                <td>{{ $grupo->nombre }}</td>
-                @can('admin.grupo.show')
-                    <td><a class="accion" href="{{ route('grupos.show', $grupo) }}">Ver</a></td>
-                @endcan
-                @can('admin.grupo.edit')
-                    <td><a class="accion" href="{{ route('grupos.edit', $grupo) }}">Editar</a></td>
-                @endcan
-                @can('admin.grupo.destroy')
-                    <td>
-                        <form action="{{ route('grupos.destroy', $grupo) }}" method="POST"
-                            onsubmit="return confirm('¿Estás seguro de que deseas eliminar este grupo?');">
-                            @csrf
-                            @method('DELETE')
-                            <input class="accion" type="submit" name="idCategoria" value="Eliminar">
-                        </form>
-                    </td>
-                @endcan
-            </tr>
-        @empty
-            <tr>
-                <td>No hay grupos</td>
-            </tr>
-        @endforelse
+            @forelse ($grupos as $grupo)
+                <tr>
+                    <td>{{ $grupo->nombre }}</td>
+                    @can('admin.grupo.show')
+                        <td><a class="accion" href="{{ route('grupos.show', $grupo) }}">Ver</a></td>
+                    @endcan
+                    @can('admin.grupo.edit')
+                        <td><a class="accion" href="{{ route('grupos.edit', $grupo) }}">Editar</a></td>
+                    @endcan
+                    @can('admin.grupo.destroy')
+                        <td>
+                            <form action="{{ route('grupos.destroy', $grupo) }}" method="POST"
+                                onsubmit="return confirm('¿Estás seguro de que deseas eliminar este grupo?');">
+                                @csrf
+                                @method('DELETE')
+                                <input class="accion" type="submit" name="idCategoria" value="Eliminar">
+                            </form>
+                        </td>
+                    @endcan
+                </tr>
+            @empty
+                <tr>
+                    <td>No hay grupos</td>
+                </tr>
+            @endforelse
 
-    </table>
-
+        </table>
+    </div>
 @endsection
