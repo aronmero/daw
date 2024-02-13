8080fff08e5fc8107a0b301e674e3ead5ef401fe
aronmero <aronmedinasp@gmail.com>
2024-02-13 12:41:36 +0000

    Router conf

--- UT8/final/src/main.js
+++ UT8/final/src/main.js
@@ -1,31 +1,20 @@
-import { createApp } from 'vue';
-import { createRouter, createWebHistory } from "vue-router";
-import './style.css';
-import './styles/style.scss';
-import App from './App.vue';
+import { createApp } from "vue";
+import "./style.css";
+import "./styles/style.scss";
+import router from "@/router/index.js";
+import App from "./App.vue";
 
 async function enableMocking() {
-    if (process.env.NODE_ENV !== "development") {
-      return;
-    }
-    const { worker } = await import("@/mocks/browser");
-  
-    return worker.start();
+  if (process.env.NODE_ENV !== "development") {
+    return;
   }
-  
-  const routes = [
-    { path: "/", component: () => import('@/views/home.vue')  },
-    { path: "/login", component: () => import('@/views/home.vue')  },
-  ];
-  const router = createRouter({
-    history: createWebHistory(),
-    routes,
-  });
+  const { worker } = await import("@/mocks/browser");
 
+  return worker.start();
+}
 
 enableMocking().then(() => {
-    const app = createApp(App);
-    app.use(router);
-    app.mount("#app");
-  });
-  
+  const app = createApp(App);
+  app.use(router);
+  app.mount("#app");
+});
