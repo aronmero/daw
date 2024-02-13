3fa87fa27c4190aa245b4bdb928604d3eb1b8637
aronmero <aronmedinasp@gmail.com>
2024-02-13 12:32:26 +0000

    Mock activado

--- UT8/final/src/main.js
+++ UT8/final/src/main.js
@@ -8,7 +8,7 @@ async function enableMocking() {
     if (process.env.NODE_ENV !== "development") {
       return;
     }
-    const { worker } = await import("./mocks/browser");
+    const { worker } = await import("@/mocks/browser");
   
     return worker.start();
   }
--- UT8/final/src/mocks/handlers.js
+++ UT8/final/src/mocks/handlers.js
@@ -18,5 +18,5 @@ export const handlers = [
     return HttpResponse.json({
       usuarios,
     });
-  }),
+  })
 ];
