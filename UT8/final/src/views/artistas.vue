b0f003753cd437b4315fa768bc1e6e7fcf40c87b
aaron <aaron@daw.com>
2024-02-23 15:15:59 +0000

    artistas agrupados

--- UT8/final/src/views/artistas.vue
+++ UT8/final/src/views/artistas.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useArtworkStore } from "@/stores/artwork";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import Header from "@/components/header.vue"
 import Card from "@/components/card.vue";
 import { apiArtworks } from "@/Api/api.js";
@@ -21,22 +21,55 @@ if (!redirectLogin()) {
   }
 }
 
+const groupedArtworks = computed(() => {
+  const grouped = {};
+  dataArtworks.value.data.forEach((artwork) => {
+    const artist = artwork.artist_title || "Desconocido";
+    if (!grouped[artist]) {
+      grouped[artist] = [];
+    }
+    grouped[artist].push(artwork);
+  });
+  return grouped;
+});
+
+const sortedArtists = computed(() => {
+  const artists = Object.keys(groupedArtworks.value);
+  return artists.sort((a, b) => groupedArtworks.value[b].length - groupedArtworks.value[a].length);
+});
 </script>
 <template>
   <Header />
   <section v-if="dataArtworks.data !== undefined">
     <h1>{{ dataArtworks.data.length }} obras </h1>
     <div class="card_container">
-      <Card v-for="artwork in dataArtworks.data" :img_id="artwork.image_id" :alt_text="artwork.thumbnail.alt_text"
-        :tituloObra="artwork.title" :autor="artwork.artist_title" :fecha="artwork.date_display"></Card>
+      <template v-for="artist in sortedArtists" :key="artist">
+        <h2>{{ artist }}</h2>
+        <div class="card_group">
+          <Card v-for="artwork in groupedArtworks[artist]" :key="artwork.id" :img_id="artwork.image_id"
+            :alt_text="artwork.thumbnail.alt_text" :tituloObra="artwork.title" :autor="artwork.artist_title"
+            :fecha="artwork.date_display"></Card>
+        </div>
+      </template>
     </div>
   </section>
 </template>
 <style scoped>
-.card_container {
+h2 {
+  text-align: left;
+  margin-left: 50px;
+}
+
+.card_group {
   display: flex;
-  flex-direction: row;
   flex-wrap: wrap;
   justify-content: center;
+}
+
+.card_container {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: center;
 }
 </style>
